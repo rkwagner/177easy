b7d27bc6dce12ffc0e54696630e28f89dc872879
Ryan Wagner <rkwagner@ucsd.edu>
2014-08-26 09:38:08 -0700

    First commit (starter code)

---
+++ qsort.py
@@ -0,0 +1,10 @@
+'''
+Author:		Ryan Wagner
+Date:		August 26, 2014
+Description:	Quicksort() program in python for dailyprogrammer #177.
+I/O:		User inputs a string of numbers to sort,
+		Program outputs the resulting sorted string.
+'''
+
+string=int(raw_input("Input a string of numbers: "))
+print ">%i" % (string) 

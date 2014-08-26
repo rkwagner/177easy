d3b3e62aaef9767113657acd4481cfe31d8f818b
Ryan Wagner <rkwagner@ucsd.edu>
2014-08-26 11:02:36 -0700

    Challenge #177 complete.

---
+++ README.md
@@ -0,0 +1,11 @@
+Description
+-----------
+Home-brewed quicksort function used in the C library, executing a non-
+modular functionality.
+
+I/O
+---
+Use "python qsort.py" to execute the program (no arguments).
+Inside the program, type an integer to be sorted by quicksort.  Typing 666
+will exit the program.
+
--- qsort.py
+++ qsort.py
@@ -4,7 +4,32 @@ Date:		August 26, 2014
 Description:	Quicksort() program in python for dailyprogrammer #177.
 I/O:		User inputs a string of numbers to sort,
 		Program outputs the resulting sorted string.
+Credits:	L/R recursion logic by /u/CatatonicMan
 '''
+counter = 0
+def _quicksort_( int_list ):
+	global counter
+	counter += 1
+	if( len( int_list ) <= 1 ):
+		return int_list
 
-string=int(raw_input("Input a string of numbers: "))
-print ">%i" % (string) 
+	pivot = int_list[ ( len( int_list ) // 2 ) ]
+
+	l = [ i for i in int_list if i < pivot ]
+	r = [ i for i in int_list if i > pivot ]
+	c = [ i for i in int_list if i == pivot ]
+	return _quicksort_( l ) + c + _quicksort_( r )
+
+while True:	
+	try:
+		int_string = str( raw_input( "Enter a number (666 exits): " ) )
+		int_list = [ int( i ) for i in int_string ]
+		if int( int_string ) != 666:
+			print( _quicksort_( int_list ) )
+			print "Sorted in %i iterations." % ( counter )
+			counter = 0
+		else:
+			print "Exit condition met."
+			break
+	except ValueError:
+		print( "Enter a valid integer value." )
